935235769475d36df5acbba82ea0f0cbe7e8ece7
ViTj4 <remi.ryckebusch.pro@gmail.com>
2023-03-14 11:56:51 +0100

    push fonctionnel de la mise en écoute du serveur

--- Serveur.py
+++ Serveur.py
@@ -1,11 +1,12 @@
 from pyftpdlib.handlers import FTPHandler
-from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+
 
 class MyFTPHandler(FTPHandler):
     authorizer = DummyAuthorizer()
-    authorizer.add_user("epsi", "client22", "/path/to/home", perm="elradfmwMT")
-    authorizer.add_anonymous("/path/to/public")
+    authorizer.add_user("epsi", "client22", "./home/epsi", perm="elradfmwMT")
+    authorizer.add_anonymous("./home/epsi")
 
     def on_connect(self):
         print(f"Client {self.remote_ip} connected")
@@ -25,11 +26,7 @@ class MyFTPHandler(FTPHandler):
     def on_file_received(self, file):
         print(f"File {file} received")
 
-handler = MyFTPHandler()
-handler.authorizer = MyFTPHandler.authorizer
-handler.banner = "Welcome to my FTP server"
-handler.passive_ports = range(60000, 65535)
-handler.timeout = 600
 
+handler = MyFTPHandler
 server = FTPServer(("127.0.0.1", 21), handler)
 server.serve_forever()
